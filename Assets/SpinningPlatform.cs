3951c5987cf342287bf3f3207e95da5d899239a1
Francesco Benelle <francesco.benelle@mail.polimi.it>
2024-10-16 11:34:42 +0200

    Circle Spinning Platform

---
+++ Assets/SpinningPlatform.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 10f *Time.deltaTime, 0f, Space.Self);
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
