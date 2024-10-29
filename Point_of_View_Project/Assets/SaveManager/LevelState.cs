82259dfcf61b15104cda22a6328a0550adce29c0
Francesco Benelle <francesco.benelle@mail.polimi.it>
2024-10-29 20:43:21 +0100

    Started the implementation of a local Save system

---
+++ Point_of_View_Project/Assets/SaveManager/LevelState.cs
@@ -0,0 +1,66 @@
+﻿namespace SaveManager
+{
+    public class LevelState
+    {
+        int levelNumber;
+        bool completed = false;
+        string time = "00:00";
+        int stars = 0;
+        private int starsCameraSwitches = 0;
+        
+        
+        public LevelState(int levelNumber, bool completed, string time, int stars, int starsCameraSwitches)
+        {
+            this.levelNumber = levelNumber;
+            this.completed = completed;
+            this.time = time;
+            this.stars = stars;
+            this.starsCameraSwitches = starsCameraSwitches;
+        }
+        
+        public int GetLevelNumber()
+        {
+            return levelNumber;
+        }
+        
+        public bool GetCompleted()
+        {
+            return completed;
+        }
+        
+        public string GetTime()
+        {
+            return time;
+        }
+        
+        public int GetStars()
+        {
+            return stars;
+        }
+        
+        public int GetStarsCameraSwitches()
+        {
+            return starsCameraSwitches;
+        }
+        
+        public void SetCompleted(bool completed)
+        {
+            this.completed = completed;
+        }
+        
+        public void SetTime(string time)
+        {
+            this.time = time;
+        }
+        
+        public void SetStars(int stars)
+        {
+            this.stars = stars;
+        }
+        
+        public void SetStarsCameraSwitches(int starsCameraSwitches)
+        {
+            this.starsCameraSwitches = starsCameraSwitches;
+        }
+    }
+}
---
+++ Point_of_View_Project/Assets/SaveManager/Preferences.cs
@@ -0,0 +1,58 @@
+﻿namespace SaveManager
+{
+    public class Preferences
+    {
+        private float masterVolume;
+		private float sfxVolume;
+        private float musicVolume;
+        private string playerName;
+        
+        public Preferences(float masterVolume, float sfxVolume, float musicVolume, string playerName)
+        {
+            this.masterVolume = masterVolume;
+            this.sfxVolume = sfxVolume;
+            this.musicVolume = musicVolume;
+            this.playerName = playerName;
+        }
+        
+        public float GetMasterVolume()
+        {
+            return masterVolume;
+        }
+        
+        public float GetSFXVolume()
+        {
+            return sfxVolume;
+        }
+        
+        public float GetMusicVolume()
+        {
+            return musicVolume;
+        }
+        
+        public string GetPlayerName()
+        {
+            return playerName;
+        }
+        
+        public void SetMasterVolume(float volume)
+        {
+            masterVolume = volume;
+        }
+        
+        public void SetSFXVolume(float volume)
+        {
+            sfxVolume = volume;
+        }
+        
+        public void SetMusicVolume(float volume)
+        {
+            musicVolume = volume;
+        }
+        
+        public void SetPlayerName(string name)
+        {
+            playerName = name;
+        }
+    }
+}
