0d0ec2872869d91d290bc5ba86f80201729a2077
jakeeuu <125929621+jakeeuu@users.noreply.github.com>
2024-11-13 10:41:49 +0100

    pressure plate sound oncollisionenter

--- Point_of_View_Project/Assets/Game/Scripts/PressurePlateManager.cs
+++ Point_of_View_Project/Assets/Game/Scripts/PressurePlateManager.cs
@@ -7,15 +7,20 @@ public class PressurePlateManager : MonoBehaviour
 {
    Renderer rend;
    Boolean _isActive;
+   public AudioClip soundEffect; // Assegna il suono dal tuo progetto.
+   private AudioSource audioSource;
 
    private void Start()
    {
       rend = GetComponent<Renderer>();
+      audioSource = gameObject.AddComponent<AudioSource>();
+      audioSource.clip = soundEffect;
    }
 
    private void OnCollisionEnter(Collision other)
    {
       rend.material.color = Color.red;
+      audioSource.Play();
       _isActive = true;
    }
    
