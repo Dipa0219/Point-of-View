9fac9567b96f1663b97f11aa3255a6e667b8177a
BurnoutBear <simone.frazzei@mail.polimi.it>
2024-11-15 11:14:19 +0100

    Trying to fix groundcheck

--- Point_of_View_Project/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
+++ Point_of_View_Project/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -1,60 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace SciFi_Warehouse_Kit.Demo.Scripts
 {
-
-    public CharacterController controller;
-    public float speed = 8f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    Vector3 velocity;
-    bool isGrounded;
-
-    
-     public AudioClip footStepSound;
-     public float footStepDelay;
- 
-     private float nextFootstep = 0;
-
-    // Update is called once per frame
-    void Update()
+    public class PlayerMovement : MonoBehaviour
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y <0)
-            {
-            velocity.y = -2f;
+        [SerializeField] private CharacterController controller;
+        [SerializeField] private float speed = 8f;
+        [SerializeField] private float gravity = -9.81f;
+        [SerializeField] private float jumpHeight = 3f;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private float groundDistance = 0.4f;
+        [SerializeField] private LayerMask groundMask;
+        private Vector3 _velocity;
+        private bool _isGrounded;
+        
+        [SerializeField] private AudioClip footStepSound;
+        [SerializeField] private float footStepDelay;
+ 
+        private float _nextFootstep;
+
+        // Update is called once per frame
+        private void Update() {
+            
+            //_isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            _isGrounded = _velocity.y == 0;
+            
+            if (_isGrounded) {
+                Debug.Log("ground check");
+            }
+            else {
+                Debug.Log("Oof");
             }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+            if (_isGrounded && _velocity.y <0) {
+                _velocity.y = -2f;
+            }
 
-        Vector3 motion = transform.right * x + transform.forward * z;
-        controller.Move(motion * speed * Time.deltaTime);
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+            Vector3 motion = transform.right * x + transform.forward * z;
+            controller.Move(motion * (speed * Time.deltaTime));
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+            if(Input.GetButtonDown("Jump") && _isGrounded) {
+                _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
 
-         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
-            {
-             nextFootstep -= Time.deltaTime;
-             if (nextFootstep <= 0) 
-                {
-                 GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
-                 nextFootstep += footStepDelay;
+            if (!_isGrounded) {
+                _velocity.y += gravity * Time.deltaTime;
+            }
+
+            controller.Move(_velocity * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && _isGrounded) {
+                _nextFootstep -= Time.deltaTime;
+                if (_nextFootstep <= 0) {
+                    GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+                    _nextFootstep += footStepDelay;
                 }
-             }
-         }
+            }
+        }
+    }
 }
 
 
