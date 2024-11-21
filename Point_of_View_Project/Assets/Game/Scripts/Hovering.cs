86fd140eff9bef4d5231b4a6772465b5342a2eef
BurnoutBear <simone.frazzei@mail.polimi.it>
2024-11-21 15:27:16 +0100

    Player prefab fixing

--- Point_of_View_Project/Assets/Game/Scripts/Hovering.cs
+++ Point_of_View_Project/Assets/Game/Scripts/Hovering.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Timers;
 using UnityEngine;
 
 namespace Game.Scripts
 {
     public class Hovering : MonoBehaviour
     {
-    
+        [SerializeField] private float amplitude;
+        [SerializeField] private float speed;
+        private float _timer = 0f;
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            transform.Translate(0, Mathf.Sin(_timer * speed / 2 * Mathf.PI) * amplitude / 1000, 0);
+        }
     }
 }
