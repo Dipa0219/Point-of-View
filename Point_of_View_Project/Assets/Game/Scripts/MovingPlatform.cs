daff6a4372d4966e4cf8cb411297203567316d3a
BurnoutBear <simone.frazzei@mail.polimi.it>
2024-11-29 13:58:06 +0100

    Platform behaviours

--- Point_of_View_Project/Assets/Game/Scripts/MovingPlatform.cs
+++ Point_of_View_Project/Assets/Game/Scripts/MovingPlatform.cs
@@ -5,15 +5,25 @@ namespace Game.Scripts
 {
     public class MovingPlatform : MonoBehaviour
     {
+        private enum FinalBehaviour
+        {
+            Stop,
+            Loop,
+            Reverse
+        }
         
         [SerializeField] private Transform[] waypoints;
         [SerializeField] private float speed;
+        [SerializeField] private FinalBehaviour finalBehaviour;
         
         private List<Transform> _children;
         private BoxCollider[] _colliders;
-        private Transform _playerTransform;
+        // private Transform _playerTransform;
         private int _currentWaypoint;
-        private bool _canMove;
+        private int _nextWaypoint = 1;
+        private bool _move;
+        private bool _stop;
+        private bool _forward = true;
         
         private void Awake()
         {
@@ -40,7 +50,7 @@ namespace Game.Scripts
             Debug.Log("ENTER");
             
             // Take transform of the player
-            _playerTransform = other.transform;
+            //_playerTransform = other.transform;
             
             // Activate colliders
             foreach (BoxCollider boxCollider in _colliders)
@@ -55,32 +65,69 @@ namespace Game.Scripts
             if (!other.CompareTag("Player")) return;
             Debug.Log("EXIT");
         }
-        
+
         private void GoToNextWaypoint()
         {
-            _canMove = true;
+            _move = true;
         }
 
         private void Update()
         {
-            if (!_canMove || _currentWaypoint >= waypoints.Length) return;
-            
-            // Debug.Log("MOVING");
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[_currentWaypoint + 1].position, speed * Time.deltaTime);
-            
-            if (transform.position == waypoints[_currentWaypoint + 1].position)
-            {
-                if (_currentWaypoint == 0)
-                    _currentWaypoint--;
-                else
-                    _currentWaypoint++;
-                
-                _canMove = false;
-                
+            if (_stop)
                 // Deactivate colliders
                 foreach (BoxCollider boxCollider in _colliders)
                     boxCollider.enabled = false;
+            
+            if (_stop || !_move || _currentWaypoint >= waypoints.Length) return;
+            
+            // Debug.Log("MOVING");
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[_nextWaypoint].position, speed * Time.deltaTime);
+
+            if (transform.position != waypoints[_currentWaypoint + 1].position) return;
+
+            // Deactivate colliders
+            switch (finalBehaviour) {
+                case FinalBehaviour.Stop:
+                    if (_nextWaypoint == waypoints.Length - 1)
+                        _stop = true;
+                    else {
+                        _currentWaypoint++; _nextWaypoint++;
+                    }
+                    break;
+                case FinalBehaviour.Loop:
+                    if (_nextWaypoint == waypoints.Length - 1)
+                        _nextWaypoint = 0;
+                    else {
+                        _currentWaypoint++; _nextWaypoint++;
+                    }
+                    break;
+                case FinalBehaviour.Reverse:
+                    if (_forward)
+                        if (_nextWaypoint == waypoints.Length - 1) {
+                            _forward = false;
+                            _currentWaypoint++; _nextWaypoint--;
+                        }
+                        else {
+                            _currentWaypoint++; _nextWaypoint++;
+                        }
+                    else {
+                        if (_nextWaypoint == waypoints.Length - 1) {
+                            _forward = false;
+                            _currentWaypoint++; _nextWaypoint--;
+                        }
+                        else {
+                            _currentWaypoint++; _nextWaypoint++;
+                        }
+                    }
+                    break;
+                
+                default:
+                    _stop = true;
+                    break;
             }
+
+            foreach (BoxCollider boxCollider in _colliders)
+                boxCollider.enabled = false;
         }
     }
 }
