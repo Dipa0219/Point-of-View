e1005437b28ed3aef47a2e6aed0da314b08b94b4
Antonio Di Paola <126001143+Dipa0219@users.noreply.github.com>
2024-12-06 16:10:13 +0100

    Initial camera 0-2, added command UI 0-1 and fixed boxes movement

--- Point_of_View_Project/Assets/Game/Scripts/BoxFix.cs
+++ Point_of_View_Project/Assets/Game/Scripts/BoxFix.cs
@@ -4,7 +4,8 @@ namespace Game.Scripts
 {
     public class BoxFix : MonoBehaviour
     {
-        [SerializeField] private float vertical = -1f;
+        [SerializeField] private float vertical = 0.001f;
+        [SerializeField] private float verticalSpeed = 0.0025f;
         [SerializeField] private float speed = 10.0f;
         public LayerMask groundLayer; // LayerMask to detect the ground
         public int gridResolution = 5; // How many rays to cast along one axis
@@ -18,6 +19,8 @@ namespace Game.Scripts
         }
          private void Update()
          {
+             transform.position = Vector3.MoveTowards(transform.position,
+                 transform.position + new Vector3(0, vertical, 0), Time.deltaTime * verticalSpeed);
              if(ShouldFall())
              {
                  transform.position = Vector3.MoveTowards(transform.position,
