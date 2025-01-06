608acc746add7037d30d5b23fcce2a9c6bba5751
Simone Frazzei <99398152+BurnoutBear@users.noreply.github.com>
2025-01-06 17:53:03 +0100

    IWTKMS

--- Point_of_View_Project/Assets/Game/Scripts/MovingPlatform.cs
+++ Point_of_View_Project/Assets/Game/Scripts/MovingPlatform.cs
@@ -1,4 +1,6 @@
-using System.Collections.Generic;
+//using System.Collections.Generic;
+
+using System.Collections;
 using UnityEngine;
 
 namespace Game.Scripts
@@ -16,19 +18,24 @@ namespace Game.Scripts
         [SerializeField] private float speed;
         [SerializeField] private FinalBehaviour finalBehaviour;
         [SerializeField] private bool continuousMovement;
-        [SerializeField] private bool isDouble;
+        [SerializeField] private bool isDouble; // If it needs to wait for two robots and not just one
         
         [SerializeField] private AudioClip soundEffect;
         private AudioSource _audioSource;
         
-        private List<Transform> _children;
-        private BoxCollider[] _colliders;
+        // private List<Transform> _children;
+        // private BoxCollider[] _colliders;
         private Transform _playerTransform;
-        private int _nextWaypoint = 1;
+        private int _nextWaypointIndex = 1;
         private bool _move;
         private bool _stop;
         private bool _forward = true;
-        private int _numPlayer;
+        // private int _numPlayer;
+        
+        private Transform _previousWaypoint;
+        private Transform _nextWaypoint;
+        private float _timeToWaypoint;
+        private float _elapsedTime;
         
         private void Start()
         {
@@ -36,11 +43,12 @@ namespace Game.Scripts
             _audioSource.clip = soundEffect;
         }
         
+        /*
         private void Awake()
         {
             _children = new List<Transform>();
             GetRecursiveChildren(transform);
-            _colliders = _children[0].GetComponents<BoxCollider>();
+            // _colliders = _children[0].GetComponents<BoxCollider>();
         }
         
         private void GetRecursiveChildren(Transform parentTransform)
@@ -54,100 +62,115 @@ namespace Game.Scripts
                 }
             }
         }
-
+        */
+        
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
             
-            //if(_numPlayer==0 && isDouble)
-            if(isDouble)
-            {
-                if (_numPlayer == 0)
-                {
-                    _numPlayer++;
-                    return;
-                }
-                
-                _numPlayer++;
-                GoToNextWaypoint();
-                return;
-            }
-            // Take transform of the player
-            //_playerTransform = other.transform;
-            //_playerTransform.SetParent(transform);
-            _numPlayer++;
+            // if(_numPlayer==0 && isDouble)
+            // if(isDouble)
+            // {
+            //     if (_numPlayer == 0)
+            //     {
+            //         _numPlayer++;
+            //         return;
+            //     }
+            //     
+            //     _numPlayer++;
+            //     GoToNextWaypoint();
+            //     return;
+            // }
+            
+            // Vector3 playerGlobalPosition = other.transform.position;
+            Debug.Log("Player Transform Before Parenting: " + other.transform);
+            //other.transform.localScale = new Vector3(4f, 1f, 4f);
+            other.transform.SetParent(transform, true);
+            //other.transform.localScale = new Vector3(4f, 4f, 4f);
+            Debug.Log("Player Position After Parenting: " + other.transform.position);
+            
+            // _numPlayer++;
+            
             // Activate colliders
-            foreach (BoxCollider boxCollider in _colliders)
-                boxCollider.enabled = true;
+            // foreach (BoxCollider boxCollider in _colliders)
+            //     boxCollider.enabled = true;
             
             // Move
+            StartCoroutine(DelayCoroutine());
             GoToNextWaypoint();
         }
         
          private void OnTriggerExit(Collider other)
          {
              if (!other.CompareTag("Player")) return;
-
-             _numPlayer--;
+             other.transform.SetParent(null);
+             // _numPlayer--;
          }
 
         private void GoToNextWaypoint()
         {
-            if (!_move)
+            if (!_move && waypoints.Length > 0)
             {
                 _audioSource.Play();
+                CalcWaypointDistanceTime();
                 _move = true;
             }
             
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if (_stop)
             {
                 // Deactivate colliders
-                foreach (BoxCollider boxCollider in _colliders)
-                    boxCollider.enabled = false;
+                // foreach (BoxCollider boxCollider in _colliders)
+                //     boxCollider.enabled = false;
                 _audioSource.Stop();
             }
                 
                 
             if (_stop || !_move) return;
             
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[_nextWaypoint].position, speed * Time.deltaTime);
+            _elapsedTime += Time.deltaTime;
+            float elapsedPercentage = _elapsedTime / _timeToWaypoint;
+            elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage);
+            transform.position = Vector3.Lerp(_previousWaypoint.position, _nextWaypoint.position, elapsedPercentage);
+            
+            // transform.position = Vector3.MoveTowards(transform.position, waypoints[_nextWaypointIndex].position, speed * Time.deltaTime);
 
-            if (transform.position != waypoints[_nextWaypoint].position) return;
+            
+            if (transform.position != waypoints[_nextWaypointIndex].position) return;
 
             switch (finalBehaviour) {
                 case FinalBehaviour.Stop:
-                    if (_nextWaypoint == waypoints.Length - 1)
+                    if (_nextWaypointIndex == waypoints.Length - 1)
                     {
                         _stop = true;
                         
                     }else
-                        _nextWaypoint++;
+                        _nextWaypointIndex++;
                     break;
                 case FinalBehaviour.Loop:
-                    if (_nextWaypoint == waypoints.Length - 1)
-                        _nextWaypoint = 0;
+                    if (_nextWaypointIndex == waypoints.Length - 1)
+                        _nextWaypointIndex = 0;
                     else
-                         _nextWaypoint++;
+                         _nextWaypointIndex++;
                     break;
                 case FinalBehaviour.Reverse:
                     if (_forward)
-                        if (_nextWaypoint == waypoints.Length - 1) {
+                        if (_nextWaypointIndex == waypoints.Length - 1) {
                             _forward = false;
-                            _nextWaypoint--;
+                            _nextWaypointIndex--;
                         }
                         else
-                             _nextWaypoint++;
+                             _nextWaypointIndex++;
                     else
-                        if (_nextWaypoint == waypoints.Length - 1) {
+                        if (_nextWaypointIndex == waypoints.Length - 1) {
                             _forward = false; 
-                            _nextWaypoint--;
+                            _nextWaypointIndex--;
                         }
                         else
-                            _nextWaypoint++;
+                            _nextWaypointIndex++;
                     break;
                 
                 default:
@@ -155,14 +178,35 @@ namespace Game.Scripts
                     break;
             }
             
+            if (elapsedPercentage >= 1)
+            {
+                CalcWaypointDistanceTime();
+            }
+            
             if (continuousMovement) return;
             
             _move = false;
             
-            foreach (BoxCollider boxCollider in _colliders)
-                boxCollider.enabled = false;
+            // foreach (BoxCollider boxCollider in _colliders)
+            //     boxCollider.enabled = false;
             
+        }
+        
+        private void CalcWaypointDistanceTime()
+        {
+            _previousWaypoint = waypoints[_nextWaypointIndex-1];
+            _nextWaypoint = waypoints[_nextWaypointIndex];
+
+            _elapsedTime = 0;
             
+            float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _nextWaypoint.position);
+            _timeToWaypoint = distanceToWaypoint / speed;
+        }
+
+        private static IEnumerator DelayCoroutine()
+        {
+            // Aspetta 3 secondi
+            yield return new WaitForSeconds(3f);
         }
     }
 }
