db87472c365f367631171283bafae926ec88a39f
Simone Frazzei <99398152+BurnoutBear@users.noreply.github.com>
2025-01-10 20:39:42 +0100

    Damn

--- Point_of_View_Project/Assets/MultiPlatformManager2.cs
+++ Point_of_View_Project/Assets/MultiPlatformManager2.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using Game.Scripts;
 using UnityEngine;
 
 public class MultiPlatformManager2 : MonoBehaviour
 {
-    [SerializeField] private GameObject Platform;
+    [SerializeField] private GameObject platform;
+    [SerializeField] private GameObject platform2;
     
     private void Start() {
-        Platform.GetComponent<MovingPlatformUnbound>().StartMoving();
+        platform.GetComponent<MovingPlatformUnbound>().StartMoving();
+        platform2.GetComponent<MovingPlatformUnbound>().StartMoving();
     }
 }
